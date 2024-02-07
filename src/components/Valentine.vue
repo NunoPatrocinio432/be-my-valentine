1663dcf334f761c88c0249b82bf6ee63045f902c
Nuno Patrocinio <nuno.patrocinio432@gmail.com>
2024-02-07 18:50:05 +0000

    Main page

--- src/components/Valentine.vue
+++ src/components/Valentine.vue
@@ -1,3 +1,7 @@
+
 <template>
-    Be my valentine
+    <h1><b>Do you want to be my Valentine?</b></h1>
+    <img src="https://media1.tenor.com/m/KdYQAufJITUAAAAC/bingus-bingus-flower.gif">
+    <button class="button-romantic" role="button">YES</button>
+    <button class="button-romantic" role="button">NO</button>
 </template>
